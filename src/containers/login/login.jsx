4dd48581a85cd72497439187e7ebccf19dfb52a3
liangbeibei <1241920375@qq.com>
2018-08-13 08:50:17 +0800

    updata

--- src/containers/login/login.jsx
+++ src/containers/login/login.jsx
@@ -2,11 +2,52 @@
 用户登陆的路由组件
  */
 import React, {Component} from 'react'
+import {NavBar,List,WingBlank,WhiteSpace,InputItem,Radio,Button}from'antd-mobile'
+
+import Logo from '../../components/logo/logo'
+
 
 export default class Login extends Component {
+    //定义初始化状态
+    state = {
+        username:'',
+        password:'',
+    }
+
+    //跳转到注册
+    toRegister = ()=>{
+        this.props.history.replace('/register')
+    }
+    //请求登陆
+    login = () =>{
+        console.log(this.state)
+    }
+    //更新状态
+    handleChange=(name,val)=>{
+        this.setState({
+            [name]:val
+        })
+    }
+
     render () {
         return (
-            <div>login</div>
+            <div>
+                <NavBar>用户登陆</NavBar>
+                <Logo/>
+                <WingBlank>
+                    <List>
+                        <InputItem type='text' placeholder='请输入用户名'
+                                   onChange={(val) => this.handleChange('username', val)}>用户名: </InputItem>
+                        <WhiteSpace/>
+                        <InputItem type='password' placeholder='请输入密码'
+                                   onChange={(val) => this.handleChange('password', val)}>密码: </InputItem>
+                        <WhiteSpace/>
+                        <Button type='primary' onClick={this.login}>登&nbsp;&nbsp;陆</Button>
+                        <WhiteSpace/>
+                        <Button onClick={this.toRegister}>没有账户</Button>
+                    </List>
+                </WingBlank>
+            </div>
         )
     }
 }
--- src/containers/register/register.jsx
+++ src/containers/register/register.jsx
@@ -2,6 +2,11 @@
 用户注册的路由组件
  */
 import React, {Component} from 'react'
+import {NavBar,List,WingBlank,WhiteSpace,InputItem,Radio,Button}from'antd-mobile'
+
+import Logo from '../../components/logo/logo'
+
+
 
 export default class Register extends Component {
     state={
@@ -9,31 +14,48 @@ export default class Register extends Component {
         password:'',
         password2:'',
         type:'dashen'
-    }
-    login = ()=>{
-        this.props.history.replace('./logo')
-    }
+    };
+
+    //跳转登陆
+    toLogin = ()=>{
+        this.props.history.replace('/login')
+    };
+    //请求登陆
+    register = ()=>{
+        console.log(this.state);
+    };
+    //更新状态
+    handleChange = (name,val)=>{
+        this.setState({
+            [name]:val
+        })
+    };
+
+
     render() {
-        const {type}=this.store;
+        const {type}=this.state;
         return (
             <div>
                 <NavBar>用户注册</NavBar>
                 <Logo/>
                 <WingBlank>
                     <List>
-                        <InputItem type='text' placeholder='请输入用户名'>用户名: </InputItem>
+                        <InputItem type='text' placeholder='请输入用户名'
+                                   onChange={(val) => this.handleChange('username', val)}>用户名: </InputItem>
                         <WhiteSpace/>
-                        <InputItem type='password' placeholder='请输入密码'>密码: </InputItem>
+                        <InputItem type='password' placeholder='请输入密码'
+                                   onChange={(val) => this.handleChange('password', val)}>密码: </InputItem>
                         <WhiteSpace/>
-                        <InputItem type='password' placeholder='请输入确认密码'>确认密码: </InputItem>
+                        <InputItem type='password' placeholder='请输入确认密码'
+                                   onChange={(val) => this.handleChange('password2', val)}>确认密码: </InputItem>
                         <WhiteSpace/>
                         <List.Item>
                             <span>用户类型: </span>&nbsp;&nbsp;&nbsp;
-                            <Radio>老板</Radio>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                            <Radio>大神</Radio>
+                            <Radio checked={type==='laoban'} onChange={() => {this.handleChange('type', 'laoban')}}>老板</Radio>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            <Radio checked={type==='dashen'} onChange={() => {this.handleChange('type', 'dashen')}}>大神</Radio>
                         </List.Item>
                         <WhiteSpace/>
-                        <Button type='primary'>注&nbsp;&nbsp;册</Button>
+                        <Button type='primary' onClick={this.register}>注&nbsp;&nbsp;册</Button>
                         <WhiteSpace/>
                         <Button onClick={this.toLogin}>已有账户</Button>
                     </List>
